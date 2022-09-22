7abf67a3a0759527c62d246e27af2d01fdeac597
JotFormDB <jotform.database1@gmail.com>
2022-09-22 16:59:10 +0800

    trying their library

--- js/mytest.js
+++ js/mytest.js
@@ -1,1 +1,8 @@
 console.log("loaded");
+
+function callbk(res){
+  console.log(res);
+}
+
+JF.initialize( {apiKey: "d17748747ac8e76618def4b967347ec5"} ); 
+JF.getFormSubmissions("222338948374870",callbk);
