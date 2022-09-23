a3c066a23bfe169286f14b68f463fbf5226d3bae
JotFormDB <jotform.database1@gmail.com>
2022-09-23 11:33:53 +0800

    updated with submission lookup
    stautus is the final status
    details is the current process

--- js/mytest.js
+++ js/mytest.js
@@ -1,7 +1,25 @@
 function callbk(res){
-  console.log(res);
+  //console.log(res);
 }
 
-var formid = "222338948374870"
+//5381705172429391458
 
-$.getJSON("https://vicom.jotform.com/API/form/" + formid + "/submissions?apiKey=d17748747ac8e76618def4b967347ec5&callback=?",callbk);
+var formid = "222338948374870";
+
+$('#subdown').mousedown(function(){
+  var tmp = $.getJSON("https://vicom.jotform.com/API/submission/" + $('#subid').val() + "?addWorkflowStatus=1&callback=?",function(res){
+    console.log(res["content"]);
+    $('#status').html(res["content"]["workflowStatus"]);
+    try{
+      $('#details').html(res["content"]["workflowStatusDetails"]["text"]);
+    }
+    catch(exception){
+      $('#details').html("");
+    }
+    
+    //console.log(tmp.content["workflowStatusDetails"]["text"]);
+  });
+  
+})
+
+$.getJSON("https://vicom.jotform.com/API/form/" + formid + "/submissions?apiKey=d17748747ac8e76618def4b967347ec5&addWorkflowStatus=1&callback=?",callbk);
