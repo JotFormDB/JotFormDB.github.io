258ac28b0a7cf6ce0d5f35f3913be49880db69ab
JotFormDB <jotform.database1@gmail.com>
2022-09-23 10:01:21 +0800

    change to use getJSON

--- js/mytest.js
+++ js/mytest.js
@@ -3,6 +3,9 @@ console.log("loaded");
 function callbk(res){
   console.log(res);
 }
-
+/*
 JF.initialize( {apiKey: "d17748747ac8e76618def4b967347ec5"} ); 
+console.log(JF);
 JF.getFormSubmissions("222338948374870",callbk);
+*/
+$.getJSON("https://vicom.jotform.com/API/form/222338948374870/submissions?apiKey=d17748747ac8e76618def4b967347ec5",callbk);
