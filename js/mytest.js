0845e1f53088df5e65137c39cb124356abff5f3e
JotFormDB <jotform.database1@gmail.com>
2022-09-23 11:44:47 +0800

    can this work

--- js/mytest.js
+++ js/mytest.js
@@ -2,13 +2,8 @@ function callbk(res){
   console.log(res);
 }
 
-//5381705172429391458
-
-var formid = "222338948374870";
-
-$('#subdown').mousedown(function(){
-  var tmp = $.getJSON("https://vicom.jotform.com/API/submission/" + $('#subid').val() + "?addWorkflowStatus=1&callback=?",function(res){
-    console.log(res["content"]);
+function statuscallback(res){
+  console.log(res["content"]);
     $('#status').html(res["content"]["workflowStatus"]);
     try{
       $('#details').html(res["content"]["workflowStatusDetails"]["text"]);
@@ -16,10 +11,15 @@ $('#subdown').mousedown(function(){
     catch(exception){
       $('#details').html("");
     }
-    
-    //console.log(tmp.content["workflowStatusDetails"]["text"]);
-  });
-  
-})
+}
+
+//5381705172429391458
+
+var formid = "222338948374870";
+
+$('#subdown').mousedown(function(){
+  $.getJSON("https://vicom.jotform.com/API/submission/" + $('#subid').val() + "?addWorkflowStatus=1&callback=?",statuscallback)
+  }
+  )
 
 $.getJSON("https://vicom.jotform.com/API/form/" + formid + "/submissions?apiKey=d17748747ac8e76618def4b967347ec5&addWorkflowStatus=1&callback=?",callbk);
