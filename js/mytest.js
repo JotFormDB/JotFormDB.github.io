fb6977e14e147da2de564ed23114cc71d490c2c6
JotFormDB <jotform.database1@gmail.com>
2022-09-23 10:10:27 +0800

    fixed by adding this callback=? not sure why it fixed it

--- js/mytest.js
+++ js/mytest.js
@@ -1,11 +1,7 @@
-console.log("loaded");
-
 function callbk(res){
   console.log(res);
 }
-/*
-JF.initialize( {apiKey: "d17748747ac8e76618def4b967347ec5"} ); 
-console.log(JF);
-JF.getFormSubmissions("222338948374870",callbk);
-*/
-$.getJSON("https://vicom.jotform.com/API/form/222338948374870/submissions?apiKey=d17748747ac8e76618def4b967347ec5",callbk);
+
+var formid = "222338948374870"
+
+$.getJSON("https://vicom.jotform.com/API/form/" + formid + "/submissions?apiKey=d17748747ac8e76618def4b967347ec5&callback=?",callbk);
